0b1e0a603c041042d15ddcefcc6cef4da69d6ae5
Sankar Prasad Biswas <sankar@collegify.com>
2019-08-16 20:29:29 +0530

    master: working with express

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 const express = require('express');
 const app = express();
+
+
+app.get('/' , (req,res) => {
+    res.send('Hello express app');
+});
+
+app.get('/api/courses' , (req,res) => {
+
+
+    // res.send('Hello express app');
+    res.send([1,2,3]);
+
+});
+
+app.get('/api/courses/:id/:year' , (req,res) => {
+    // res.send(req.params.id);
+    // res.send(req.query);
+
+})
+
+//PORT
+
+const port = process.env.PORT || 4050
+
+app.listen(port , () => {
+    console.log('Listening port ' , port);
+});
